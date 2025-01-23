2662eb805d10f434d296ce2a14eb477766503b41
毛煜东 <maoyudong@192.168.2.45>
2025-01-23 18:13:42 +0800

    add prompt_maker init

---
+++ ragas/configuration/promptmaker.py
@@ -0,0 +1,49 @@
+from ragas.configuration.base import *
+# import ragas
+import ConfigSpace
+from ConfigSpace import ConfigurationSpace, ForbiddenEqualsClause, ForbiddenAndConjunction, ForbiddenInClause, NotEqualsCondition
+from ConfigSpace.hyperparameters import CategoricalHyperparameter, UniformIntegerHyperparameter, UniformFloatHyperparameter, Constant
+
+method_zoo = ['fstring', 'window_replacement', 'long_context_reorder']
+
+
+class PromptMakerConfiguration(BaseConfiguration):
+
+    def __init__(self, config: Dict, project_dir: Optional[str] = None):
+        super().__init__(config, project_dir)
+        self.cs = self.build(config)
+
+    def build(self, config) -> ConfigurationSpace|None:
+        prompt_maker = config["prompt_maker"]
+        if prompt_maker is not None:
+            method = []
+            prompt_set = []
+            try:
+                for iter in prompt_maker["method"]:
+                    if iter in method_zoo:
+                        method.append(iter)
+                for k,v in prompt_maker["prompt"].items():
+                    prompt_set.append(v)
+            except:
+                raise ValueError(
+				f"Prompt Maker elements are missing."
+			)
+
+
+                
+            cs = ConfigurationSpace(
+                space={
+                    "prompt_maker_methods": CategoricalHyperparameter("methods", choices=method),
+                    "init_prompt": CategoricalHyperparameter("init_prompt", choices=prompt_set),
+                }
+            )
+
+            return cs
+        else:
+            return None
+
+    def sampling(self, size: Optional[int] = 1) -> Union[Configuration, List[Configuration]]:
+        return self.cs.sample_configuration(size)
+
+
+
